51df2741c7610f261d9b53b2748907d9bf1d73e3
shenlong3030 <shenlong3030@gmail.com>
2022-09-13 20:39:40 +0700

    Update laz.js

--- laz.js
+++ laz.js
@@ -204,8 +204,8 @@ setTimeout(function() {
         $(tb).find('td').html(function(index, html) {
             return html.replace('Ốp lưng', 'Ốp') // xóa chữ 'lưng' trong tên SP
                 .replace('[HCM]', '') // xóa [HCM] trong tên SP
-                .replace(/Nhóm Màu:([^,-]+)/g, '<br>Màu:<strong>$1</strong>')
-                .replace(/Dòng sản phẩm tương thích:([^,-]+)/g, '<br>Dòng:<strong>$1</strong>');
+                .replace(/Nhóm Màu:([^,-]+)/, '<br>Màu:<font color="red">$1</font>')
+                .replace(/Dòng sản phẩm tương thích:([^,-]+)/, '<br>Dòng:<font color="red">$1</font>');
 
         });
         $(tb).tablesorter();
